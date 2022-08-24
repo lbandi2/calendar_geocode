74975295545e41feae3c07399f620eff57b1cd6c
Sergio Schmilovich <sergio.schmilovich@gmail.com>
2022-08-24 16:13:30 -0500

    update run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 
 mkdir -p logs
 cd /home/sergio/scripts/calendar_geocode/
+find logs -type f -mtime +30 -delete
 . ./env/bin/activate
 /home/sergio/scripts/calendar_geocode/env/bin/python3 /home/sergio/scripts/calendar_geocode/main.py >> logs/calendar_geocode-"`date +"%Y-%m-%d_%H.%M.%S"`".log 2>&1
 deactivate
