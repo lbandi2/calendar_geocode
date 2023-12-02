f6ba26b71bad2320f485c3fee3b1cacdf2c77f12
Sergio Schmilovich <sergio.schmilovich@gmail.com>
2023-12-01 20:00:04 -0500

    update

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from geocode import Calendar
 import os
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
 
-API_KEY = os.getenv('GOOGLE_API')
-CAL_ID = os.getenv('CALENDAR_ID')
+API_KEY = os.getenv('API_KEY')
+CAL_ID = os.getenv('CAL_ID')
 
 Calendar(CAL_ID, API_KEY)
